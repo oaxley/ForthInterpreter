3cad207e49cb4be0270792befacdb79f1dff1660
slegrand <ut5svsxgc@relay.firefox.com>
2025-03-19 23:41:10 -0400

    Create basic main

---
+++ src/main.cc
@@ -0,0 +1,32 @@
+/*
+ * @file    main.cc
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point
+ */
+
+// ----- includes
+#include "ForthInterpreter.h"
+
+#include <iostream>
+
+
+// ----- main
+int main() {
+    ForthInterpreter forth;
+    std::string input;
+
+    std::cout << "Forth Interpreter. Enter 'exit' to quit.\n";
+
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, input);
+
+        if (input == "exit")
+            break;
+
+        forth.run(input);
+    }
+
+    return 0;
+}
