b6c93c5d67d1f5a2d3e491ae540cce4aab21da66
slegrand <ut5svsxgc@relay.firefox.com>
2025-03-31 22:48:21 -0400

    Add basic tokenizer for user input

---
+++ src/tokenizer.cc
@@ -0,0 +1,77 @@
+/*
+ * @file    tokenizer.cc
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Implementation / Forth interpreter tokenizer
+ */
+
+// ----- includes
+#include "tokenizer.h"
+
+// ----- public implementation
+
+// constructor
+Tokenizer::Tokenizer()
+{
+}
+
+// destructor
+/*virtual*/ Tokenizer::~Tokenizer()
+{
+}
+
+/* Initialize the underlying istream with the user input
+ * Args:
+ *      line (std::string) : the input line from the user
+ */
+void Tokenizer::parse(const std::string& line)
+{
+    // set the underlying string, and ensure we start at the beginning
+    stream_.str(line);
+    stream_.seekg(0L);
+
+    // empty a potential list of unread tokens
+    next_token_.clear();
+}
+
+/* return the next token in the string
+ * Returns:
+ *      The next token if any, std::nullopt if none is available
+ */
+std::optional<std::string> Tokenizer::next()
+{
+    std::string token;
+
+    // lookup in the list if we already have a token from a previous peek
+    if (next_token_.size() > 0) {
+        // retrieve the token and send it back
+        token = next_token_.front(); next_token_.pop_front();
+        return token;
+    }
+
+    // end of the stream
+    if (stream_.eof())
+        return std::nullopt;
+
+    // retrieve a new token
+    stream_ >> token;
+    return token;
+}
+
+/* peek at the next token, without removing it from the queue
+ * Returns:
+ *      The next token if any, std::nullopt if none is available
+ */
+std::optional<std::string> Tokenizer::peek()
+{
+    // no more token to read
+    if (stream_.eof())
+        return std::nullopt;
+
+    // grab the next token and add it to the list
+    std::string token;
+    stream_ >> token;
+    next_token_.push_back(token);
+
+    return token;
+}
---
+++ src/tokenizer.h
@@ -0,0 +1,40 @@
+/*
+ * @file    tokenizer.h
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Interface / Forth interpreter tokenizer
+ */
+
+// ----- header guards
+#ifndef FORTH_TOKENIZER_H_
+#define FORTH_TOKENIZER_H_
+
+// ----- includes
+#include <string>
+#include <optional>
+#include <sstream>
+#include <list>
+
+// ----- class
+class Tokenizer
+{
+public:
+    Tokenizer();
+    virtual ~Tokenizer();
+
+    // no copy or move semantics
+    Tokenizer(const Tokenizer&) = delete;
+    Tokenizer& operator=(const Tokenizer&) = delete;
+    Tokenizer(Tokenizer&&) = delete;
+    Tokenizer&& operator=(Tokenizer&&) = delete;
+
+    void parse(const std::string&);
+    std::optional<std::string> next();
+    std::optional<std::string> peek();
+
+private:
+    std::istringstream stream_ {};
+    std::list<std::string> next_token_ {};
+};
+
+#endif // FORTH_TOKENIZER_H_
