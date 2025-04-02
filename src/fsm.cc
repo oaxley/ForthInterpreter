dab7ce7c271cd068927939c0c52701aa2a2fdfd9
slegrand <ut5svsxgc@relay.firefox.com>
2025-04-01 22:27:38 -0400

    Draft implementation for FSM class

---
+++ src/fsm.cc
@@ -0,0 +1,96 @@
+/*
+ * @file    fsm.h
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Interface / Finite State Machine
+ */
+
+// ----- includes
+#include "fsm.h"
+
+// ----- begin namespace
+namespace FSM {
+
+// constructor
+Engine::Engine()
+{
+}
+
+// destructor
+/*virtual*/ Engine::~Engine()
+{
+}
+
+// setup the FSM
+void Engine::setup()
+{
+}
+
+/* add a transition to the FSM
+ * Args:
+ *      transition : the transition to add to the FSM
+ */
+void Engine::add(const Transition& transition)
+{
+}
+
+/* add a list of transitions to the FSM
+ * Args:
+ *      transitions : the list of transitions to add
+ */
+void Engine::add(std::list<Transition> transitions)
+{
+}
+
+/* get the current state of the FSM
+ * Returns:
+ *      the name of the current state
+ */
+std::string_view Engine::state() const
+{
+    return current_.name;
+}
+
+// start the FSM
+void Engine::start()
+{
+
+}
+
+// stop the FSM
+void Engine::stop()
+{
+
+}
+
+// update the FSM with a new event
+void Engine::update(const Event& event)
+{
+
+}
+
+/* check if a transition to a state is possible
+ * Args:
+ *      state : the target state to check
+ * Returns:
+ *      True if the transition is possible, False otherwise
+ */
+bool Engine::can(const State& state) const
+{
+    return false;
+}
+
+/* check if a transition to a state is NOT possible
+ * Args:
+ *      state : the target state to check
+ * Returns:
+ *      True if the transition is not possible, False otherwise
+ */
+bool Engine::cannot(const State& state) const
+{
+    return false;
+}
+
+
+// ----- end namespace
+}
